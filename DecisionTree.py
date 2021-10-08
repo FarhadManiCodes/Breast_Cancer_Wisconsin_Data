00bedcf318daee894b94a3a7ae5aaa570ebf6393
Farhad Mani <frhdmani@gmail.com>
2021-10-08 19:07:50 +0100

    Add Node object

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,8 +1,37 @@
 """ Decision Tree Classifier"""
-from typing import Union
+from typing import Tuple, Union
 import pandas as pd
 import numpy as np
 from math import log2
+from dataclasses import dataclass
+
+@dataclass
+class threshold_result:
+    index:int
+    measure:float
+
+def entropy(labels):
+    """ Computes entropy of label distribution. """
+    p = labels.sum()/len(labels)
+    if 0 < p < 1:
+        return -p * log2(p) - (1 - p) * log2(1 - p)
+    else:
+        return 0     # Compute entropy
+
+
+def findtreshold(y_sorted:np.ndarray,criterion:str) -> threshold_result:
+    if criterion == 'entropy':
+        min_entropy = 1
+        threshold_index = 1
+        label_size=len(y_sorted)
+        for i in range(1,label_size):
+            ent = ((i) * entropy(y_sorted[:i]) +
+                   (label_size-i) * entropy(y_sorted[i:]))/label_size
+            if ent < min_entropy:
+                min_entropy = ent
+                threshold_index = i
+
+        return threshold_result(threshold_index, min_entropy)
 
 
 class Node:
@@ -16,15 +45,40 @@ class Node:
                 *,criterion = "entropy"):
         self.X = X
         self.y = y
+        self.criterion = criterion
+        self.processed = False
 
     def __str__(self) -> str:
-        return "Node (class)"
+        if self.processed:
+            return f"""Node object Processed: 
+            - current {self.criterion} : {self.entropy}
+            - best column for spliting: {self.best_col}
+            - threshold : {self.threshold}
+            - {self.criterion} after the split : {self.measure}
+                    """
+        else:
+            return "Unprocessed Node"
 
     def find_the_best_feature(self):
-        for col in range(self.X.shape[1]):
-            pass       
-
-
+        min_measure = 1.0
+        best_col = None
+        threshold = None
+        if self.criterion == "entropy":
+            self.entropy = entropy(self.y)
+        if self.entropy > 0:
+            for i,col in enumerate(self.X.T):
+                sorted_ind = np.argsort(col)
+                y_sorted = self.y[sorted_ind]
+                result= findtreshold(y_sorted,self.criterion)
+                if result.measure < min_measure:
+                    min_measure = result.measure
+                    best_col = i
+                    threshold = (col[sorted_ind][result.index-1] +
+                                col[sorted_ind][result.index]) / 2
+        self.best_col = best_col
+        self.threshold = threshold
+        self.measure = min_measure
+        self.processed = True
 
 class DecisionTreeClassifier:
     """
@@ -65,12 +119,14 @@ class DecisionTreeClassifier:
 
     def fit(self,
             X:pd.DataFrame,
-            y:Union[np.ndarray,pd.Series]):
+            y:np.ndarray):
         depth = 0
         if self.max_depth:
             max_depth = self.max_depth
         else:
             max_depth = int(log2(len(y)))
         while (depth < max_depth):
-            print('test')
+            N = Node(X.to_numpy(),y)
+            N.find_the_best_feature()
+            print(N)
             depth += 1
